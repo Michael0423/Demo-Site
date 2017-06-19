8bed9042c3e0ff7c9617bc48d9e77cd493f8c373
Michael <ms800423@gmail.com>
2017-06-19 10:57:51 +0800

    1

--- include/js/config.js
+++ include/js/config.js
@@ -1,10 +1,19 @@
 $(function(){
 	// $.getJSON();
-	var a = ["A", "B", "C", "D"];
+	var a = [];
+	// for (var i=0; i<100; i++) {
+	// 	a[i]=i;
+	// }
 	var demoList = $("#demo-list");
+	var demoContent = $("#demo-content");
+	var count=1;
 	$.each(a, function(index, content){
-		var item = $("<div>").append($("<ul>").append($("<il>").append($("<a>").attr("href","#").text(content))));
+		var item = $("<div>").attr("id", "list-"+count).append($("<ul>").append($("<il>").append($("<a>").attr("href","#").text(content))));
+		var item2 = $("<div>").attr("id", "content-"+count).append($("<ul>").append($("<il>").append($("<a>").attr("href","#").text(content))));
 		demoList.append(item);
+		demoContent.append(item2);
+		count++;
 	});
 	// $("#demo-list").html("ASDASD");
+	// console.log($("#demo-list").find("#list-1"));
 });
---
+++ router/fileTest.php
@@ -0,0 +1,46 @@
+<?php
+
+$filename = '../source/JPG.jpg';
+// header("Content-type: image/JPEG",true);
+// echo "透過fread方式讀取圖片檔，再轉base64：";
+
+$image = fread(fopen($filename,r), filesize($filename));
+$base64 = 'data:image/jpb=g;base64,' . base64_encode($image);
+// echo "<img src='".$base64."' />";
+echo $base64;
+// fclose($image);
+
+
+echo "<br/>";
+echo "<br/>";
+
+// echo "透過PHP API方式讀取圖片檔,再轉base64:";
+
+$percent = 0.5;
+
+// Get new sizes
+list($width, $height) = getimagesize($filename);
+$newwidth = $width * $percent;
+$newheight = $height * $percent;
+
+// Load
+$thumb = imagecreatetruecolor($newwidth, $newheight);
+$source = imagecreatefromjpeg($filename);
+
+// Resize
+imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+// Output
+// imagejpeg($thumb);
+// echo $thumb;
+
+ob_start(); // Let's start output buffering.
+    imagejpeg($thumb); //This will normally output the image, but because of ob_start(), it won't.
+    $thumb = ob_get_contents(); //Instead, output above is saved to $contents
+ob_end_clean(); //End the output buffer.
+
+//
+$base64 = 'data:image/jpb=g;base64,' . base64_encode($thumb);
+// echo "<img src='".$base64."' />";
+echo $base64;
+?>
