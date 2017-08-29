b2db45a055676a8b493bede77216dc74b885caca
Michael <ms800423@gmail.com>
2017-08-29 23:18:38 +0800

    套件引入架構調整

--- include/js/config.js
+++ include/js/config.js
@@ -1,19 +1,18 @@
 $(function(){
-	// $.getJSON();
-	var a = [];
-	// for (var i=0; i<100; i++) {
-	// 	a[i]=i;
-	// }
-	var demoList = $("#demo-list");
-	var demoContent = $("#demo-content");
-	var count=1;
-	$.each(a, function(index, content){
-		var item = $("<div>").attr("id", "list-"+count).append($("<ul>").append($("<il>").append($("<a>").attr("href","#").text(content))));
-		var item2 = $("<div>").attr("id", "content-"+count).append($("<ul>").append($("<il>").append($("<a>").attr("href","#").text(content))));
-		demoList.append(item);
-		demoContent.append(item2);
-		count++;
-	});
-	// $("#demo-list").html("ASDASD");
-	// console.log($("#demo-list").find("#list-1"));
+	// console.log("ASD");
+	var ajaxOption = {
+		url: "./router/login.php",
+		type: "POST",
+		success: function(rs){
+			// alert(rs);
+		},
+		error: function(e){
+			// alert(e);
+		}
+	};
+	$.ajax(ajaxOption);
 });
+
+function A(){
+	alert("A is called!");
+}
--- router/fileTest.php
+++ router/fileTest.php
@@ -1,18 +1,18 @@
 <?php
 
-$filename = '../source/JPG.jpg';
+$filename = '../source/JPG2.jpg';
 // header("Content-type: image/JPEG",true);
 // echo "透過fread方式讀取圖片檔，再轉base64：";
 
 $image = fread(fopen($filename,r), filesize($filename));
 $base64 = 'data:image/jpb=g;base64,' . base64_encode($image);
 // echo "<img src='".$base64."' />";
-echo $base64;
+// echo $base64;
 // fclose($image);
 
 
-echo "<br/>";
-echo "<br/>";
+// echo "<br/>";
+// echo "<br/>";
 
 // echo "透過PHP API方式讀取圖片檔,再轉base64:";
 
@@ -33,14 +33,20 @@ imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $he
 // Output
 // imagejpeg($thumb);
 // echo $thumb;
-
+echo microtime();
+echo "<br>";
 ob_start(); // Let's start output buffering.
     imagejpeg($thumb); //This will normally output the image, but because of ob_start(), it won't.
     $thumb = ob_get_contents(); //Instead, output above is saved to $contents
 ob_end_clean(); //End the output buffer.
+echo microtime();
 
 //
-$base64 = 'data:image/jpb=g;base64,' . base64_encode($thumb);
+$base64 = 'data:image/jpb=g;base64,' . base64_encode($image);
+$base642 = 'data:image/jpb=g;base64,' . base64_encode($thumb);
 // echo "<img src='".$base64."' />";
-echo $base64;
+echo "<img src='".$base642."' />";
+echo strlen($base64);
+echo "<br>";
+echo strlen($base642);
 ?>
